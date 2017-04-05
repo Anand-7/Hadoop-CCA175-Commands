249f38a2111cca9a43b42051fa871f339410df17
varunu28 <varun.u28@gmail.com>
2017-04-05 06:12:24 +0530

    Added first scala class

---
+++ New Syllabus/02 Scala - REPL, Programming basics and Object Oriented Concepts/fraction_class_scala.scala
@@ -0,0 +1,6 @@
+class Fraction(num: Int, den: Int){	
+	 println("Num is " + num)	
+	 println("Den is " + den)	
+	 println("Fraction is " + num + "/" + den)	
+	 println("Fraction is " + num.toFloat/den)	
+	 }
